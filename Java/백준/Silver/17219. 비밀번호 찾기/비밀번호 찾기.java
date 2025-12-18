a810e7a9c14b1de87603611473f3e45b6f55c3be
k22900 <132814310+k22900@users.noreply.github.com>
2025-12-18 19:56:42 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 1064 ms, Memory: 59756 KB -BaekjoonHub

--- Java/백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ Java/백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -1,21 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        StringTokenizer st;
         Map<String, String> passwordList = new HashMap<>();//사이트목록
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String num = br.readLine();
-        StringTokenizer l = new StringTokenizer(num);
-        int n = Integer.valueOf(l.nextToken());
-        int m = Integer.valueOf(l.nextToken());
+        st = new StringTokenizer(num);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
-            StringTokenizer st = new StringTokenizer(line);
+            st = new StringTokenizer(line);
             String siteName = st.nextToken();
             String password = st.nextToken();
             passwordList.put(siteName, password);
